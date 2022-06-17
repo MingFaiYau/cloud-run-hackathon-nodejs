c6e4ae0f9e6b0681983f6d58adaca69f26828786
Ding.MF.Yau <Ding.MF.Yau@pccw.com>
2022-06-17 15:15:29 +0800

    Update web.js

--- web.js
+++ web.js
@@ -1,19 +1,51 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
 app.get('/', function (req, res) {
-  res.send('Let the battle begin!');
-});
+	res.send('Let the battle begin!')
+})
+
+/*
+
+curl -d '{
+  "_links": {
+    "self": {
+      "href": "https://foo.com"
+    }
+  },
+  "arena": {
+    "dims": [4,3],
+    "state": {
+      "https://foo.com": {
+        "x": 0,
+        "y": 0,
+        "direction": "N",
+        "wasHit": false,
+        "score": 0
+      }
+    }
+  }
+}' -H "Content-Type: application/json" -X POST -w "\n" \
+  <THE URL YOU COPIED>
+
+*/
 
 app.post('/', function (req, res) {
-  const response = JSON.stringify(req.body)
-  console.log('response',response);
+	const response = JSON.stringify(req.body)
 
-  const moves = ['F', 'T', 'L', 'R'];
-  res.send(moves[Math.floor(Math.random() * moves.length)]);
-});
+	const selfHref = response._links.self.href
+	const arena = response.arena
+	const state = arena.state
+	const mapSize = arena.dims
 
-app.listen(process.env.PORT || 8080);
+	const self = state[selfHref]
+	console.log('mine', self)
+
+	const moves = ['F', 'T', 'L', 'R']
+	res.send(moves[Math.floor(Math.random() * moves.length)])
+})
+
+app.listen(process.env.PORT || 8080)
